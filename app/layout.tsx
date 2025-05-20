5800769a8b76d4b0dc5f2537fd4ef03b0457a598
artempaskall <artem.yogi@gmail.com>
2025-05-20 21:31:21 +0300

    refactor globals.scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import "../app/styles/globals.module.scss"
-// import "../app/styles/reset.scss"
+import "../app/styles/globals.scss"
+import "../app/styles/reset.scss"
 import { Montserrat, Inter } from "next/font/google"
 import Head from "next/head"
 import { SessionProvider } from "next-auth/react"
@@ -15,15 +15,6 @@ const inter = Inter({
   weight: ["400", "700"],
   variable: "--font-inter",
 })
-
-// export const metadata = {
-//   description: `Пориньте у світ витонченого ремесла з нашою колекцією преміальних,
-//    ручно виготовлених вин. Кожна пляшка розповідає історію відданості,
-//    пристрасті та майстерності. Відкрийте для себе ексклюзивні вина,
-//    що пропонують унікальні смаки та аромати.
-//    Незалежно від того, чи ви досвідчений дегустатор,
-//    чи просто любитель, наша колекція додасть елегантності та розкошів кожній події.`,
-// }
 
 export default function RootLayout({
   children,
--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,12 +1,12 @@
 import st from "./header.module.scss"
-import global from "@/app/styles/globals.module.scss"
+import "@/app/styles/globals.scss"
 import React from "react"
 import Link from "next/link"
 
 export default function Header() {
   return (
     <div className={st["header"]}>
-      <div className={global["wrapp-1200"]}>
+      <div className={"wrapp-1200"}>
         <div className={st["header-wrapp"]}>
           <div> + Add product</div>
           <Link href="/register">register</Link>
