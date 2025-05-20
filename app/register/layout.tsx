98e90899c687840437e3fdc9499013f619c598b5
artempaskall <artem.yogi@gmail.com>
2025-05-20 21:39:00 +0300

    check

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next"
 import Register from "../register/page"
 
 export const metadata: Metadata = {
-  title: "Registration Page",
+  title: "Registration Page ewrgwergwerwegr",
 }
 
 export default function RegistrationPage() {
