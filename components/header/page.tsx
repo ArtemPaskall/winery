9607ab75a788999a876995eb1035f8f4265c60a1
ArtemPaskall <artempasskal@gmail.com>
2025-06-01 00:56:05 +0300

    implement user info

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,16 +1,21 @@
+"use client"
 import st from "./header.module.scss"
 import "@/app/styles/globals.scss"
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
+import { useSession } from "next-auth/react"
 
 export default function Header() {
+  const { data: session } = useSession()
+  const user = session?.user ?? null
+  console.log(user)
+
   return (
     <div className={st["header"]}>
       <div className={"wrapp-1200"}>
         <div className={st["header-wrapp"]}>
           <Link href={"/"}>
-            {" "}
             <Image
               src={"/winery.png"}
               alt="головний логотип"
@@ -18,7 +23,20 @@ export default function Header() {
               height={50}
             />
           </Link>
-          <Link href="/register">register</Link>
+          {!user ? (
+            <Link href="/register">Вхід</Link>
+          ) : (
+            <Link href={"/register"} className={st["avatar-wrapp"]}>
+              <Image
+                src={`${user.image}`}
+                alt="головний логотип"
+                width={50}
+                height={50}
+                className={st["user-img"]}
+              />
+              <p className={st["user-name"]}>{user.name}</p>
+            </Link>
+          )}
         </div>
       </div>
     </div>
