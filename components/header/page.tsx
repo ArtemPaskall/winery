5e1c2620f48ac46eb8c06e4778ca066bbfb4b778
ArtemPaskall <artempasskal@gmail.com>
2025-06-02 21:55:15 +0300

    add header

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -12,33 +12,66 @@ export default function Header() {
   console.log(user)
 
   return (
-    <div className={st["header"]}>
-      <div className={"wrapp-1200"}>
-        <div className={st["header-wrapp"]}>
-          <Link href={"/"}>
-            <Image
-              src={"/winery.png"}
-              alt="головний логотип"
-              width={50}
-              height={50}
-            />
-          </Link>
-          {!user ? (
-            <Link href="/register">Вхід</Link>
-          ) : (
-            <Link href={"/register"} className={st["avatar-wrapp"]}>
-              <Image
-                src={`${user.image}`}
-                alt="головний логотип"
-                width={50}
-                height={50}
-                className={st["user-img"]}
-              />
-              <p className={st["user-name"]}>{user.name}</p>
-            </Link>
-          )}
+    <>
+      <div className={st["header-top"]}>
+        <div className="wrapp-1200">
+          <div className={st["header-top-wrapp"]}>
+            <div></div>
+            <div className={st["percent-wrapp"]}>
+              Отримай знижку <span className={st["percent"]}>10%</span>
+            </div>
+            <div>
+              {" "}
+              {!user ? (
+                <Link href="/register">Вхід</Link>
+              ) : (
+                <Link href={"/register"} className={st["avatar-wrapp"]}>
+                  <Image
+                    src={`${user.image}`}
+                    alt="головний логотип"
+                    width={50}
+                    height={50}
+                    className={st["user-img"]}
+                  />
+                  <p className={st["user-name"]}>{user.name}</p>
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={st["header-bottom"]}>
+        <div className="wrapp-1200">
+          <div className={st["header-bottom-wrapp"]}>
+            <Link href={"/"}>
+              <Image
+                src={"/burger-menu.svg"}
+                alt="Меню"
+                width={50}
+                height={50}
+              ></Image>
+            </Link>
+            <Link href={"/"}>
+              <Image
+                src={"/winery2-removebg.png"}
+                alt="головний логотип"
+                width={200}
+                height={100}
+              />
+            </Link>
+            <Link href={"/"} className={st["cart-wrapp"]}>
+              <Image
+                src={"/cart.png"}
+                alt="Меню"
+                width={30}
+                height={30}
+                className={st["cart"]}
+              ></Image>
+              <div className={st["cart-fill"]}>0</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
