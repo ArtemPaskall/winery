09352b9cbbedeca0616c63d32ccb2ae346bed335
artempaskall <artem.yogi@gmail.com>
2025-06-08 22:02:08 +0300

    header and i18n finished

--- components/langSwitcher/page.tsx
+++ components/langSwitcher/page.tsx
@@ -1,24 +1,54 @@
 "use client"
 import { usePathname, useRouter } from "next/navigation"
 import Image from "next/image"
+import st from "./langSwitcher.module.scss"
+import { useState, useEffect, useRef } from "react"
 
 export default function LangSwitcher() {
   const router = useRouter()
   const pathname = usePathname()
-
   const currentLocale = pathname.split("/")[1]
+  const [open, setOpen] = useState(false)
+  const [selectedLang, setSelectedLang] = useState(currentLocale)
+  const ref = useRef<HTMLDivElement>(null)
 
-  const toggleLocale = () => {
-    const newLocale = currentLocale === "en" ? "uk" : "en"
-    const newPath = pathname.replace(`/${currentLocale}`, `/${newLocale}`)
+  const handleLangClick = (lang: string) => {
+    setSelectedLang(lang)
+    const newPath = pathname.replace(`/${currentLocale}`, `/${lang}`)
     router.push(newPath)
+    setOpen(false)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
   return (
-    <div onClick={toggleLocale}>
-      <Image src={"/globe.svg"} width={20} height={20} alt="Мова"></Image>
-      <div>UK</div>
-      <div>EN</div>
+    <div
+      ref={ref}
+      className={st["lang-switcher"]}
+      onClick={() => setOpen(!open)}
+    >
+      <Image src={"/globe.svg"} width={20} height={20} alt="Мова" />
+      <div className={`${st["lang-wrapp"]} ${open ? st["open"] : ""}`}>
+        {!open ? (
+          <div>{selectedLang.toUpperCase()}</div>
+        ) : (
+          <>
+            <div onClick={() => handleLangClick("UK")}>UK</div>
+            <div onClick={() => handleLangClick("RU")}>RU</div>
+            <div onClick={() => handleLangClick("EN")}>EN</div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
