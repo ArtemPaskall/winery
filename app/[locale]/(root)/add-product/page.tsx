e717c71aac65dc03cb23e4c67ea548f27fb25d1a
ArtemPaskall <artempasskal@gmail.com>
2026-02-24 23:36:50 +0200

    fix

--- app/[locale]/(root)/add-product/page.tsx
+++ app/[locale]/(root)/add-product/page.tsx
@@ -1,13 +1,14 @@
-import { getMessages } from "next-intl/server"
+// import { getMessages } from "next-intl/server"
 import AddWineForm from "./AddWineForm/page"
+import { Locale } from "../../../../types"
 
 export default async function AddWinePage({
-  params,
+  // params,
 }: {
-  params: { locale: string }
+  params: { locale: Locale }
 }) {
-  const messages = await getMessages(params.locale)
-  console.log(messages)
+  // const messages = await getMessages(params.locale)
+  // console.log(messages)
 
   return <AddWineForm />
 }
