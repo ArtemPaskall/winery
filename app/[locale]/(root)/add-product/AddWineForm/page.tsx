5fd9ad823bc07c36a94f1f0cc3b25a161db53d04
ArtemPaskall <artempasskal@gmail.com>
2026-03-01 21:54:28 +0200

    add translation to form error

--- app/[locale]/(root)/add-product/AddWineForm/page.tsx
+++ app/[locale]/(root)/add-product/AddWineForm/page.tsx
@@ -8,88 +8,6 @@ import { useTranslations } from "next-intl"
 import { useEffect, useState } from "react"
 import Image from "next/image"
 
-const wineSchema = z.object({
-  title: z.object({
-    uk: z
-      .string()
-      .trim()
-      .min(3, "Мінімум 3 символи")
-      .max(100, "Максимум 100 символів"),
-    en: z
-      .string()
-      .trim()
-      .min(3, "Minimum 3 characters")
-      .max(100, "Maximum 100 characters"),
-    ru: z
-      .string()
-      .trim()
-      .min(3, "Минимум 3 символа")
-      .max(100, "Максимум 100 символов"),
-  }),
-
-  description: z.object({
-    uk: z
-      .string()
-      .trim()
-      .min(5, "Мінімум 5 символів")
-      .max(2000, "Занадто довгий опис"),
-    en: z
-      .string()
-      .trim()
-      .min(5, "Minimum 5 characters")
-      .max(2000, "Description too long"),
-    ru: z
-      .string()
-      .trim()
-      .min(5, "Минимум 5 символов")
-      .max(2000, "Слишком длинное описание"),
-  }),
-
-  WineCategory: z.enum(
-    ["red", "white", "rose", "sparkling", "dessert", "fortified"],
-    {
-      required_error: "Оберіть категорію",
-      invalid_type_error: "Некоректна категорія",
-    }
-  ),
-
-  volume: z
-    .string({ required_error: "Обʼєм обовʼязковий" })
-    .trim()
-    .min(1, "Обʼєм обовʼязковий")
-    .refine((val) => !isNaN(Number(val)), {
-      message: "Має бути число",
-    })
-    .refine((val) => Number(val) > 0, {
-      message: "Має бути більше 0",
-    }),
-  price: z
-    .string({ required_error: "Ціна обовʼязкова" })
-    .trim()
-    .min(1, "Ціна обовʼязкова")
-    .refine((val) => !isNaN(Number(val)), {
-      message: "Має бути число",
-    })
-    .refine((val) => Number(val) > 0, {
-      message: "Має бути більше 0",
-    }),
-  imageUrl: z
-    .string()
-    .trim()
-    .url("Некоректний URL")
-    .optional()
-    .or(z.literal("")),
-})
-
-type WineFormData = z.infer<typeof wineSchema>
-
-const initialFormData: Partial<WineFormData> = {
-  title: { uk: "", en: "", ru: "" },
-  description: { uk: "", en: "", ru: "" },
-  WineCategory: "red",
-  imageUrl: "",
-}
-
 export default function AddWineForm() {
   const t = useTranslations("AddWine")
   const [loading, setLoading] = useState(false)
@@ -97,6 +15,77 @@ export default function AddWineForm() {
     type: "success" | "error"
     text: string
   } | null>(null)
+
+  const wineSchema = z.object({
+    title: z.object({
+      uk: z
+        .string()
+        .trim()
+        .min(3, { message: t("min_3_characters") })
+        .max(100, { message: t("max_100_characters") }),
+      en: z
+        .string()
+        .trim()
+        .min(3, { message: t("min_3_characters") })
+        .max(100, { message: t("max_100_characters") }),
+      ru: z
+        .string()
+        .trim()
+        .min(3, { message: t("min_3_characters") })
+        .max(100, { message: t("max_100_characters") }),
+    }),
+    description: z.object({
+      uk: z
+        .string()
+        .trim()
+        .min(5, { message: t("min_5_characters") })
+        .max(2000, { message: t("max_2000_characters") }),
+      en: z
+        .string()
+        .trim()
+        .min(5, { message: t("min_5_characters") })
+        .max(2000, { message: t("max_2000_characters") }),
+      ru: z
+        .string()
+        .trim()
+        .min(5, { message: t("min_5_characters") })
+        .max(2000, { message: t("max_2000_characters") }),
+    }),
+    WineCategory: z.enum(
+      ["red", "white", "rose", "sparkling", "dessert", "fortified"],
+      {
+        required_error: t("required_category"),
+        invalid_type_error: t("invalid_category"),
+      }
+    ),
+    volume: z
+      .string({ required_error: t("required") })
+      .trim()
+      .min(1, { message: t("required") })
+      .refine((val) => !isNaN(Number(val)), { message: t("must_be_number") })
+      .refine((val) => Number(val) > 0, { message: t("greater_than_0") }),
+    price: z
+      .string({ required_error: t("required") })
+      .trim()
+      .min(1, { message: t("required") })
+      .refine((val) => !isNaN(Number(val)), { message: t("must_be_number") })
+      .refine((val) => Number(val) > 0, { message: t("greater_than_0") }),
+    imageUrl: z
+      .string()
+      .trim()
+      .url(t("invalid_url"))
+      .optional()
+      .or(z.literal("")),
+  })
+
+  type WineFormData = z.infer<typeof wineSchema>
+
+  const initialFormData: Partial<WineFormData> = {
+    title: { uk: "", en: "", ru: "" },
+    description: { uk: "", en: "", ru: "" },
+    WineCategory: "red",
+    imageUrl: "",
+  }
 
   const {
     register,
