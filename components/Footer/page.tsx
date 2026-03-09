20be8a14d3214d4deca331e562f28f7a704034aa
ArtemPaskall <artempasskal@gmail.com>
2026-03-09 22:45:47 +0200

    footer translation

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,29 +1,34 @@
+import { getTranslations } from "next-intl/server"
 import st from "./footer.module.scss"
 import Image from "next/image"
 
-export default function Footer() {
+export default async function Footer() {
+  const t = await getTranslations("FooterPage")
+
   return (
     <footer className={st["footer"]}>
       <div className={st["footer-left"]}>
         <div className={st["footer-left-top"]}>
           <div className={st["footer-content"]}>
-            <div className={st["footer-title"]}>CONTACT</div>
+            <div className={st["footer-title"]}>{t("Contact")}</div>
             <div className={`${st["footer-text-16"]} ${st["mt30"]}`}>
               Email: info@mysite.com
             </div>
-            <div className={st["footer-text-16"]}>Tel: 123-456-7890</div>
+            <div className={st["footer-text-16"]}>{t("Tel")}: 123-456-7890</div>
             <div className={st["footer-text-16"]}>
-              Adresse: 500 Terry Francine Street SF, CA 94158
+              {t("Address")}: 500 Terry Francine Street SF, CA 94158
             </div>
           </div>
         </div>
         <div className={st["footer-left-bottom"]}>
           <div className={st["footer-content"]}>
-            <div className={st["footer-title"]}>VISIT US</div>
+            <div className={st["footer-title"]}>{t("visit-us")}</div>
             <div className={`${st["footer-text-16"]} ${st["mt30"]}`}>
-              Mon - Fri: 8am - 8pm
+              {t("mon-fri")}: 8am - 8pm
             </div>
-            <div className={st["footer-text-16"]}>Saturday: 9am - 7pm</div>
+            <div className={st["footer-text-16"]}>
+              {t("Saturday")}: 9am - 7pm
+            </div>
             <div className={`${st["footer-text-14"]} ${st["mt50"]}`}>
               © Copyright 2035
             </div>
@@ -32,14 +37,14 @@ export default function Footer() {
       </div>
       <div className={st["footer-right"]}>
         <div className={st["footer-content"]}>
-          <div className={st["footer-title"]}>SUBSCRIBE</div>
+          <div className={st["footer-title"]}> {t("Subscribe")}</div>
           <div className={`${st["footer-text-16"]} ${st["mt20"]}`}>
-            Fill a glass & subscribe
+            {t("fill-glass")}
           </div>
           <label className={`${st["checkbox"]} ${st["mt20"]}`}>
             <input type="checkbox" />
             <span className={st["checkmark"]}></span>
-            Yes, subscribe me to your newsletter.
+            {t("email-checkbox")}
           </label>
           <div className={`${st["email"]} ${st["mt20"]}`}>
             <label htmlFor="email" className={st["email-label"]}>
@@ -53,7 +58,7 @@ export default function Footer() {
                 className={st["email-input"]}
               />
               <button type="button" className={st["email-button"]}>
-                Submit
+                {t("Submit")}
               </button>
             </div>
           </div>
@@ -84,12 +89,12 @@ export default function Footer() {
             </div>
           </div>
           <div className={`${st["footer-link-wrapp"]} ${st["mt30"]}`}>
-            <div>Shipping & Returns</div>
-            <div>FAQ</div>
+            <div> {t("Shipping")}</div>
+            <div> {t("FAQ")}</div>
           </div>
           <div className={`${st["footer-link-wrapp"]} ${st["mt10"]}`}>
-            <div>Payment Methods</div>
-            <div>Store Policy</div>
+            <div>{t("Payment")}</div>
+            <div>{t("Store")}</div>
           </div>
         </div>
       </div>
